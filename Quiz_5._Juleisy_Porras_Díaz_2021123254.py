ff39869051117c094dcb297c9ca6945571bb4c3e
Juleisy Porras <80361927+Juless03@users.noreply.github.com>
2022-04-06 09:56:23 -0600

    Cambio después del stash

--- Quiz_5._Juleisy_Porras_Díaz_2021123254.py
+++ Quiz_5._Juleisy_Porras_Díaz_2021123254.py
@@ -23,3 +23,4 @@ while numeroUsuario != numeroAleatorio:
 print("¡Felicidades, has adivinado!")
 print("Hizo" ,intentos, "intentos en total")
 print("Esta es la modificación")
+print("Modificación para el stash")
